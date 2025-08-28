17b7e60118705096e4eea823fd590182cb0a75d2
milanbarini <160081138+milanbarini@users.noreply.github.com>
2025-08-28 23:15:47 +0300

    Upload

---
+++ data.js
@@ -0,0 +1,105 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-corona-camera002_post",
+      "name": "Corona Camera002_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7870041951014501,
+          "pitch": 0.21266903591264352,
+          "rotation": 0,
+          "target": "1-corona-camera003_post"
+        },
+        {
+          "yaw": -1.8043420109977149,
+          "pitch": 0.3139548465978734,
+          "rotation": 0,
+          "target": "2-corona-camera005_post"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-corona-camera003_post",
+      "name": "Corona Camera003_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-corona-camera005_post",
+      "name": "Corona Camera005_Post",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
